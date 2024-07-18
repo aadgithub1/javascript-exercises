5e496cc2a87c016e8fcae76221239fd70f94caff
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-18 11:14:23 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  if (typeof(tempInF === 'number')){
+    let tempInC = Math.round(((tempInF - 32) * (5/9)) * 10) / 10
+  
+    return tempInC
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  if (typeof(tempInC === 'number')){
+    let tempInF = Math.round(((tempInC * (9/5) + 32)) * 10) / 10
+  
+    return tempInF
+  }
 };
 
 // Do not edit below this line
