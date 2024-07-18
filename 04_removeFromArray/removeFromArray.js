b986d3061117c5d7c12dce560e3960c8d8a177cb
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-18 08:46:12 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    //you have an array
+    //you have the value of an item you want to remove 3, 'hi', etc.
+    //find the items(s) in the array
+
+    //create array fro target indices
+    //declare target el with valuesToRemove[i]
+    //iterate through array while indexOf !== -1 (the value still exists in the array / subsection)
+        //if target value is found
+        //save the index in idx, add to indices array
+        //begin searching for the same value at idx + 1
+
+    for (let i = 0; i < valuesToRemove.length; i++){
+        let targetEl = valuesToRemove[i]
+        let targetIndex = array.indexOf(targetEl)
+        while (targetIndex !== -1){
+            array.splice(targetIndex, 1)
+            targetIndex = array.indexOf(targetEl)
+        }
+    }
+    let correctedArray = array.slice()
+    return correctedArray
 };
 
 // Do not edit below this line
