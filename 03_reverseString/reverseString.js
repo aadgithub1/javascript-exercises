767810ffbe135de83f93696ecdd5b2c84960295d
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-18 06:25:58 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArr = string.split('')
+    let revArr = stringArr.reverse()
+    let revStr = revArr.join('')
+    return revStr
 
 };
+
+//split the string into array of letters
+//make a new array
+//append original array position 0 to position -1, 1 to -2, etc.
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
