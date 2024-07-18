447ce1eb9c717092e1d60e8a96ed7cdc0af713e5
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-18 10:45:54 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    //add every number from first num to second num
+    if(!Number.isInteger(firstNum) 
+    || !Number.isInteger(secondNum)
+    || firstNum < 0
+    || secondNum < 0){
+        return 'ERROR'
+    }
+    if (firstNum > secondNum){
+        let temp = firstNum
+        firstNum = secondNum
+        secondNum = temp
+    }
 
+    let accumulator = 0
+    for (let i = firstNum; i <= secondNum; i++){
+        accumulator+=i
+    }
+    return accumulator
 };
 
 // Do not edit below this line
