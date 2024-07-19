90c3d5392fea0a057bc88a08d748141177375c3c
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-19 08:47:33 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray){
+    const today = new Date()
+    //set variable equal to the first person to represent initial value for oldest person
+    let oldestPerson;
+    //set variable equal to oldest/highest age = 0
+    let highestAge = 0
+    peopleArray.forEach(person => {
+        if (!person.yearOfDeath){
+            person.yearOfDeath = today.getFullYear()
+        }
+        let age = person.yearOfDeath - person.yearOfBirth
+        if (age > highestAge){
+            highestAge = age
+            oldestPerson = person
+        }
+        
 
-};
+    });
+    return oldestPerson
+}
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
