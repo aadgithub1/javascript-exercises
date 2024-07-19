4e4c3233ce2ca7d42c9febc23c7dba00ec326c7c
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-19 06:23:04 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function(string){
+    //split the string into letters
+    let parsedLettersArray = string.toLowerCase().match(/([A-Z0-9])/gi)
+    let revArray = parsedLettersArray.slice().reverse()
 
-};
+    for (let i = 0; i < parsedLettersArray.length; i++){
+        if (parsedLettersArray[i] !== revArray[i]){
+            return false
+        }
+    }
+    return true
+}
 
 // Do not edit below this line
 module.exports = palindromes;
