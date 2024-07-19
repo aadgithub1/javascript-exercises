00650fa76ebbdb19688d147edd0c2b44981fd515
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-19 09:12:02 -0400

    Fix verbose comments

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,6 @@
 const findTheOldest = function(peopleArray){
     const today = new Date()
-    //set variable equal to the first person to represent initial value for oldest person
     let oldestPerson;
-    //set variable equal to oldest/highest age = 0
     let highestAge = 0
     peopleArray.forEach(person => {
         if (!person.yearOfDeath){
@@ -13,7 +11,6 @@ const findTheOldest = function(peopleArray){
             highestAge = age
             oldestPerson = person
         }
-        
 
     });
     return oldestPerson
