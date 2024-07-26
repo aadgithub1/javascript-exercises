04ad4d95aefaeda8e602676e5381cffa61ac4f5b
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-26 06:45:44 -0400

    Use array functions to check for palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,9 @@
 const palindromes = function(string){
-    //split the string into letters
     let parsedLettersArray = string.toLowerCase().match(/([A-Z0-9])/gi)
     let revArray = parsedLettersArray.slice().reverse()
-
-    for (let i = 0; i < parsedLettersArray.length; i++){
-        if (parsedLettersArray[i] !== revArray[i]){
-            return false
-        }
-    }
-    return true
+    return !parsedLettersArray
+    .map((letter, index) => letter === revArray[index])
+    .includes(false)
 }
-
 // Do not edit below this line
 module.exports = palindromes;
